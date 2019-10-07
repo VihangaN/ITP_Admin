8321d43b57d5a43b391adfee88113924514b6ad1
Vihanga nivarthana <31271508+VihangaN@users.noreply.github.com>
2019-10-08 04:11:15 +0530

    user access level updating  added

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-function topmenu(e){$("#top_left_menu").children().hide(),0!=e?($("#top_left_menu").animate({width:"400px"}),$("#"+e).fadeIn()):$("#top_left_menu").hide}$(document).ready(function(){});
+function topmenu(e){$("#top_left_menu").children().hide(),0!=e?($("#top_left_menu").animate({width:"450px"}),$("#"+e).fadeIn()):$("#top_left_menu").hide}$(document).ready(function(){});
+
+function student_login(){
+    $("[id='admin']").hide();
+}
+function notlog(){
+    $("#sidebar").hide();
+    $(".left").width("0%");
+    $(".right").width("100%");
+    $('#top_right').hide();
+    $('#topbar').hide();
+}
+
+function log(){
+    $("#sidebar").show();
+    $(".left").width("9%");
+    $(".right").width("91%");
+    $('#top_right').show();
+    $('#topbar').show();
+}
